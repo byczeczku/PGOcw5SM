bea26d0c498decc97de11465dd84e169c69a7dbf
byczeczku <s33867@pjwstk.edu.pl>
2025-04-14 22:57:54 +0200

    Skończyłem wszystko

--- Film.java
+++ Film.java
@@ -1,2 +1,35 @@
-public class Film {
+public class Film extends MediaBiblioteczne
+{
+    String rezyser;
+    int czasTrwania;
+
+    //
+
+    public Film( String tytul, int rokWydania, String rezyser, int czasTrwania )
+    {
+        super( tytul, rokWydania );
+        this.rezyser = rezyser;
+        this.czasTrwania = czasTrwania;
+    }
+
+    @Override
+    public void wyswietlInformacje()
+    {
+        super.wyswietlInformacje();
+        System.out.println( "Rezyser: " + this.rezyser);
+        System.out.println( "Czas trwania: " + this.czasTrwania);
+    }
+
+    public void sprawdzCzasTrwania()
+    {
+        System.out.println( "Czas trwania: " + czasTrwania);
+        if( czasTrwania > 120)
+        {
+            System.out.println( "Ten film jest dluuuuuuuuuuuuuuugi :>");
+        }
+        else
+        {
+            System.out.println( "Ten film jest standardowej dlugosci.");
+        }
+    }
 }
